6351698c370ccbf51e410f0be6bc8f28931d590a
sidthakur <sid.thakur@outlook.com>
2021-07-06 18:48:47 -0700

    Remove duplicate and add missing .terraform* pattern

--- bin/folder_contents.sh
+++ bin/folder_contents.sh
@@ -5,9 +5,9 @@ set -e
 
 # List of arguments
 build_folder=$1
-exclude_tar_dirs_terraform='--exclude=".terraform" --exclude="*.tfstate*"'
+exclude_tar_dirs_terraform='--exclude=".terraform" --exclude=".terraform*" --exclude="*.tfstate*"'
 exclude_tar_dirs_general='--exclude=".git" --exclude="tests"'
-exclude_tar_dirs_python='--exclude=".pytest_cache" --exclude="tests" --exclude="__pycache__"'
+exclude_tar_dirs_python='--exclude=".pytest_cache" --exclude="__pycache__"'
 
 which tar > /dev/null || { echo 'ERROR: tar is not installed' ; exit 1; }
 
